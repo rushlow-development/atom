17429303e1ff69e7fb0dc5d51ed6c89b9924e278
Jesse Rushlow <jr@geeshoe.com>
2020-01-14 12:09:39 -0500

    WIP - atom xml generator

---
+++ src/Generator/XMLGenerator.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * Copyright 2020 Jesse Rushlow - Geeshoe Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Geeshoe\Atom\Generator;
+
+use Geeshoe\Atom\Contract\FeedRequiredInterface;
+
+/**
+ * Class XMLGenerator
+ *
+ * @package Geeshoe\Atom\Generator
+ * @author  Jesse Rushlow <jr@geeshoe.com>
+ */
+class XMLGenerator
+{
+    protected \DOMDocument $document;
+
+    protected bool $pretty;
+
+    /**
+     * XMLGenerator constructor.
+     *
+     * @param \DOMDocument|null $document
+     * @param bool              $pretty     Return pretty XML Document
+     */
+    public function __construct(\DOMDocument $document = null, bool $pretty = false)
+    {
+        if ($document === null) {
+            $document = new \DOMDocument('1.0', 'UTF-8');
+        }
+
+        $this->document = $document;
+        $this->pretty = $pretty;
+    }
+
+    /**
+     * Create Atom 1.0 XML Feed Element
+     *
+     * @param FeedRequiredInterface $feed
+     */
+    public function initialize(FeedRequiredInterface $feed): void
+    {
+        $feedElement = $this->document->createElementNS(
+            'https://www.w3.org/2005/Atom',
+            'feed'
+        );
+
+        $feedElement->appendChild($this->createTitleElement($feed->getTitle()));
+    }
+
+    /**
+     * Get XML Document
+     *
+     * @return string
+     */
+    public function getXML(): string
+    {
+        $this->document->formatOutput = $this->pretty;
+
+        return $this->document->saveXML();
+    }
+
+    /**
+     * Create Atom title:element for use in either feed:element or entry:element
+     *
+     * @param string $title
+     * @return \DOMElement
+     */
+    public function createTitleElement(string $title): \DOMElement
+    {
+        $titleElement = $this->document->createElement('title');
+        $titleText = $this->document->createTextNode($title);
+
+        $titleElement->appendChild($titleText);
+
+        return $titleElement;
+    }
+}
---
+++ tests/UnitTests/Generator/XMLGeneratorTest.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright 2020 Jesse Rushlow - Geeshoe Development
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Geeshoe\Atom\UnitTests\Generator;
+
+use Geeshoe\Atom\Generator\XMLGenerator;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class XMLGeneratorTest
+ *
+ * @package Geeshoe\Atom\UnitTests\Generator
+ * @author  Jesse Rushlow <jr@geeshoe.com>
+ */
+class XMLGeneratorTest extends TestCase
+{
+    public function testGetXMLReturnsValidXMLDocument(): void
+    {
+        $generator = new XMLGenerator();
+
+        self::assertSame(
+            '<?xml version="1.0" encoding="UTF-8"?>' . "\n",
+            $generator->getXML()
+        );
+    }
+
+    public function testCreateTitleElementMethodCreatesDomElementWithTitleParam(): void
+    {
+        $mockElement = $this->createMock(\DOMElement::class);
+        $mockElement->expects($this->once())
+            ->method('appendChild')
+            ->with(self::isInstanceOf(\DOMNode::class))
+        ;
+
+        $mockDocument = $this->createMock(\DOMDocument::class);
+        $mockDocument->expects($this->once())
+            ->method('createElement')
+            ->with('title')
+            ->willReturn($mockElement)
+        ;
+
+        $mockDocument->expects($this->once())
+            ->method('createTextNode')
+            ->with('test')
+            ->willReturn(new \DOMNode())
+        ;
+
+        $generator = new XMLGenerator($mockDocument);
+        $generator->createTitleElement('test');
+    }
+}
