b61410c9f704e643891ebf45363d420c21a7d6c3
Jesse Rushlow <jr@geeshoe.com>
2020-01-28 04:16:34 -0500

    author is initialized in entry construct

--- src/Model/Entry.php
+++ src/Model/Entry.php
@@ -20,6 +20,7 @@ declare(strict_types=1);
 
 namespace Geeshoe\Atom\Model;
 
+use Geeshoe\Atom\Collection\ElementCollection;
 use Geeshoe\Atom\Contract\CollectionInterface;
 use Geeshoe\Atom\Contract\EntryInterface;
 use Geeshoe\Atom\Exception\ModelException;
@@ -53,6 +54,7 @@ class Entry implements EntryInterface
         $this->title = $title;
 
         $this->updated = $updated;
+        $this->author = new ElementCollection();
     }
 
     /**
@@ -96,11 +98,7 @@ class Entry implements EntryInterface
      */
     public function getAuthor(): CollectionInterface
     {
-        if (isset($this->author)) {
-            return $this->author;
-        }
-
-        throw ModelException::emptyPropertyException('Author');
+        return $this->author;
     }
 
     public function setAuthor(CollectionInterface $author): void
