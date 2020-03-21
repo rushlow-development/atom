583de179571f6bb244a255aaf38a8a113b7012ac
Jesse Rushlow <jr@geeshoe.com>
2020-03-21 10:30:15 -0400

    created collection of entries

---
+++ src/Collection/EntryCollection.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Copyright 2020 Jesse Rushlow - Geeshoe Development.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Geeshoe\Atom\Collection;
+
+use Geeshoe\Atom\Model\Entry;
+
+class EntryCollection extends AbstractCollection
+{
+    public function addEntry(Entry $entry): void
+    {
+        $this->offsetSet($entry->getId(), $entry);
+    }
+
+    public function removeEntry(Entry $entry): void
+    {
+        $this->offsetUnset($entry->getId());
+    }
+
+    public function getEntry(string $id): Entry
+    {
+        return $this->offsetGet($id);
+    }
+}
